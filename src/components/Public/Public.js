d69678d1a546be195cfbe944ba8c6368608b3808
Dennis Klarenbeek <dennisklarenbeek@192.168.0.100>
2023-04-24 21:45:15 +0200

    changed to homepage

--- src/components/Public/Public.js
+++ src/components/Public/Public.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
+import './Public.css'
+
 const Public = () => {
   return (
-    <Fragment>
-        <h1>Public Page for people who have no login</h1>
-        <Link to='/login' />
-    </Fragment>
+    <div className="public-html">
+        <div className="public-body">
+            <h1>Project MNGR</h1>
+            <div className="button-container">
+                <Link className='public-button' to='/login' >LOGIN</Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
